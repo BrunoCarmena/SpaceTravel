8de440af3789f5531f3bbed2dc0616498a37ad22
bruno <bruno.s.carmena@hotmail.com>
2023-09-03 17:29:40 -0300

    se agrego header sticky y modificaciones en estiilos e inicio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector('header');
+window.addEventListener('scroll', ()=>{
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+
+const lilink = document.querySelectorAll("header ul li a");
+const section = document.querySelectorAll("section");
+
+function activeMenu(){
+    let secLenght = section.length;
+    while(--secLenght && window.scrollY +500 < section[secLenght].offsetTop){}
+    liLink.forEach(sec => sec.classList.remove("active"));
+    liLink[secLenght].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
